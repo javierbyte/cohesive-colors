89fbcc9954ac6f9ebc858df5d190b45116dcf594
javierbyte <hi@javier.xyz>
2019-10-14 22:34:05 -0600

    Updates

---
+++ precache-manifest.8faacd1893604f0e590d3631ab2e7a8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0c961c7c578bcb4b93af62b3b114c2",
+    "url": "/cohesive-colors/index.html"
+  },
+  {
+    "revision": "0c2f0c6654a56e7129f3",
+    "url": "/cohesive-colors/static/css/main.1b755e64.chunk.css"
+  },
+  {
+    "revision": "da97698e44543fee1cb5",
+    "url": "/cohesive-colors/static/js/2.35af6b0b.chunk.js"
+  },
+  {
+    "revision": "0c2f0c6654a56e7129f3",
+    "url": "/cohesive-colors/static/js/main.2387c7f3.chunk.js"
+  },
+  {
+    "revision": "78791aae084c00e7f66f",
+    "url": "/cohesive-colors/static/js/runtime-main.88bfc42e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visual-center/precache-manifest.561a3a4c9bc25d5bb1314b3d44754505.js"
+  "/cohesive-colors/precache-manifest.8faacd1893604f0e590d3631ab2e7a8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/visual-center/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cohesive-colors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
