ef3fd548d8ef2b42f2be929a87753685d208ac7b
javierbyte <hi@javier.xyz>
2019-10-14 22:25:10 -0600

    Updates

---
+++ precache-manifest.6daf7e7eb3d0bdea36c562d250e99e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cc40c24a112f948dc3dc156913578e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f36a07abef67a19f65bf",
+    "url": "/static/css/main.1b755e64.chunk.css"
+  },
+  {
+    "revision": "403d52ca56fc08616228",
+    "url": "/static/js/2.77b7c472.chunk.js"
+  },
+  {
+    "revision": "f36a07abef67a19f65bf",
+    "url": "/static/js/main.92226519.chunk.js"
+  },
+  {
+    "revision": "fd4eb883bb6566e192fe",
+    "url": "/static/js/runtime-main.64dc9dc2.js"
+  }
+]);
