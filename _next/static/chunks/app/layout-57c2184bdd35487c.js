e9b9c644d546f916c7f804d90c3c97f8523baf14
javierbyte <javierbyte@users.noreply.github.com>
2026-08-15 09:11:27 +0000

    deploy: c94deecdcf0c9c4b1cde1b8d7bb14fd769f152c2

---
+++ _next/static/chunks/app/layout-57c2184bdd35487c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9205:function(n,e,t){Promise.resolve().then(t.t.bind(t,8469,23)),Promise.resolve().then(t.t.bind(t,9461,23))},9461:function(){},8469:function(){}},function(n){n.O(0,[878,323,744],function(){return n(n.s=9205)}),_N_E=n.O()}]);
